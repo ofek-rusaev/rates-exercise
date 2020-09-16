52af5d7281258afbc91c9a3cb803fff659054c7c
ofek-rusaev <ofek2602@gmail.com>
2020-09-16 12:33:05 +0300

    Updates

---
+++ precache-manifest.2af94de2beb5f085a3e5e64990dddad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9818cc3424f1faaa69b5019a5d5268d0",
+    "url": "/rates-exercise/index.html"
+  },
+  {
+    "revision": "795f1080c19006cb3599",
+    "url": "/rates-exercise/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "53bbbc239d299cd7ae10",
+    "url": "/rates-exercise/static/js/2.02ec724b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/rates-exercise/static/js/2.02ec724b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "795f1080c19006cb3599",
+    "url": "/rates-exercise/static/js/main.2d4cd65e.chunk.js"
+  },
+  {
+    "revision": "75934c2aec8de079f4f8",
+    "url": "/rates-exercise/static/js/runtime-main.37e85ad7.js"
+  }
+]);
